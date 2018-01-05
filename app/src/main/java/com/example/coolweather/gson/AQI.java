209e42cd3bc1564df2f6a23e87be369133663417
Your Name <you@example.com>
2018-01-05 09:35:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Bam on 2018/1/3.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
